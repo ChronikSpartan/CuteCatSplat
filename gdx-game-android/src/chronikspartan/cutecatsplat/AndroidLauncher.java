3bd539e164e5a4f887899e66089de1b8a932b6a1
ChronikSpartan <stavrospilatis@hotmail.com>
2017-02-14 13:24:16 +0000

    libgdx framework update working

--- gdx-game-android/src/chronikspartan/cutecatsplat/AndroidLauncher.java
+++ gdx-game-android/src/chronikspartan/cutecatsplat/AndroidLauncher.java
@@ -6,13 +6,7 @@ import com.badlogic.gdx.backends.android.*;
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
-		/* DEBUG
-		String str = com.badlogic.gdx.Version.VERSION;
-		System.out.println(str);
-		*/
-		
 		super.onCreate(savedInstanceState);
-
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new CuteCatSplat(), config);
 	}
